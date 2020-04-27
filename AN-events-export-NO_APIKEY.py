2ce2012416b8ec0317092e77dae9ad00d6ccb20b
teslations <teslations@gmail.com>
2020-04-27 20:39:44 +1000

    Update

--- AN-events-export-NO_APIKEY.py
+++ AN-events-export-NO_APIKEY.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Extracting Event Data from Action Network
+# Extracts Event Data from Action Network
 # for creating an ICS format calendar
 # for importing into Google Calendar
 #
@@ -14,45 +14,60 @@
 # iCalendar Validator
 # https://icalendar.org/validator.html
 #
-# NOTE:  You need to enter your API KEY and TZID in SETTINGS below to run this program
-#
-#****************************SETTINGS****************************
-#**** Enter your API KEY here
+# You need to enter your API KEY and TZID in SETTINGS below to run this program
+
+#****************SETTINGS****************
+#******** Enter your API KEY here ********
 api_key = 'ENTER YOUR API KEY HERE'
 
 
-endpoint = 'https://actionnetwork.org/api/v2/events/'
+if api_key == 'ENTER YOUR API KEY HERE':
+    api_key = input(' Enter your API KEY : ')
+    print('''Enter your API KEY in the settings section
+    of this program, to store it permanently.
+    And your TimeZone ID (TZID). ''')
 
-#**** Enter your TimeZone here
-TZID = 'Australia/Brisbane'
-#*******************************************************************
+# **** Enter your TimeZone ID here ****
+TZID = 'Australia/Brisbane'    
+    
+# **** ENDPOINT ****
+endpoint = 'https://actionnetwork.org/api/v2/events/'
+ 
+#For a collection of all events associated with your API key
+# https://actionnetwork.org/api/v2/events
+#
+#For all events associated with a specific event campaign
+# https://actionnetwork.org/api/v2/event_campaigns/[event_campaign_id]/events
+#
+#To address a specific event, use the identifier without the action_network: prefix to construct a URL, like
+# https://actionnetwork.org/api/v2/events/[id]
+# https://actionnetwork.org/api/v2/events/d91b4b2e-ae0e-4cd3-9ed7-d0ec501b0bc3
+#****************************************
+
 import requests
 import json
 import datetime
 import pytz
 
-# Retrieve a collection of event resources (GET)
 response = requests.get(endpoint,
  headers={
    "OSDI-API-Token": api_key,
  }
 )
+
 if response:
   #Request is successful
-
-  #Read the output
   data = response.text
-  
-  #Parse JSON – convert the string to JSON
   parsed = json.loads(data)
   events = parsed["_embedded"]["osdi:events"]
-  
-  #Pretty Print
-  #print(json.dumps(events, indent=4))
+  #print(json.dumps(events, sort_keys=True, indent=4))
+  #write JSON file to disk
+  with open('AN-events.json', 'w') as json_file:
+    json.dump(parsed, json_file, sort_keys=True, indent=4)
 
   #----CALENDAR----
   print ('BEGIN:VCALENDAR')
-  print ('PRODID:-//Teslations//Calendar//EN')
+  print ('PRODID:-//XR//Calendar//EN')
   print ('VERSION:2.0')
   print ('CALSCALE:GREGORIAN')
   print ('METHOD:PUBLISH')
@@ -61,40 +76,60 @@ if response:
 
   #----TIMEZONE----  
   print ('BEGIN:VTIMEZONE')
+  #3.2.19. Time Zone Identifier
+  #  The "TZID" property parameter MUST NOT be applied to TIME
+  #  properties whose time values are specified in UTC.
   print ('TZID:' + TZID)
   print ('X-LIC-LOCATION:' + TZID)
 
   #----STANDARD----
   print ('BEGIN:STANDARD')
+  # 3.8.3.3. Time Zone Offset From. This property specifies the offset that is in use prior to this time zone observance. 
   print ('TZOFFSETFROM:+1000')
+  #3.8.3.4. Time Zone Offset To . This property specifies the offset that is in use in this time zone observance.
   print ('TZOFFSETTO:+1000')
+  #3.8.3.2.  Time Zone Name
   print ('TZNAME:AEST')
   print ('DTSTART:19700101T000000Z')
   print ('END:STANDARD')
   
   print ('END:VTIMEZONE')
-
+  
+#----EVENT-----------------------------------
   for i in events:
 
     #----VEVENT----
     print ('BEGIN:VEVENT')
 
     #3.8.2.4. Date-Time Start - when the calendar component begins
+    # Dates are entered as local time in AN but exported as GMT(Z) time. ??
     orig_start_date = i['start_date']
-    d = datetime.datetime.strptime(orig_start_date, "%Y-%m-%dT%H:%M:%SZ")
-    mod_start_date = d.strftime("%Y%m%dT%H%M%SZ")
-    print ('DTSTART:' + mod_start_date)
+    if str.endswith(orig_start_date,'Z'):
+        d = datetime.datetime.strptime(orig_start_date, "%Y-%m-%dT%H:%M:%SZ")
+        mod_start_date = d.strftime("%Y%m%dT%H%M%SZ")
+        prefix_date=':'
+    else:    
+        prefix_date=';'+TZID+':'
+    print ('DSTART'+prefix_date+mod_start_date)
 
     #3.8.2.2. Date-Time End - the date and time that a calendar component ends
-    # There is no end date field in Action Network
-#    orig_end_date = i['end_date']
-#    d = datetime.datetime.strptime(orig_end_date, "%Y-%m-%dT%H:%M:%SZ")
-#    mod_end_date = d.strftime("%Y%m%dT%H%M%SZ")
-#    print ('DTEND:' + mod_end_date)
+    if 'end_date' in events:
+        orig_end_date = i['end_date']
+        if str.endswith(orig_end_date,'Z'):
+            d = datetime.datetime.strptime(orig_end_date, "%Y-%m-%dT%H:%M:%S")
+            mod_end_date = d.strftime("%Y%m%dT%H%M%S")
+            prefix_date=':'
+        else:    
+            prefix_date=';'+TZID+':'
+        print ('DSTART'+prefix_date+mod_start_date)
+    else:
+        #if no end date: end date = start date
+        print('DTEND'+prefix_date+mod_start_date)
 
     #3.8.5.3. Recurrence Rule
-    # There is no Recurrence field in Action Network
-#    print ('RRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20200508T135959Z;BYDAY=FR')
+# TO DO - Check if Action Network has recurring events
+#    if 'rrule' in events:
+#        print ('RRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20200508T135959Z;BYDAY=FR')
 
     #----Current Date Time in 20200418T001100Z format
     now = datetime.datetime.now()
@@ -107,7 +142,7 @@ if response:
     print ('DTSTAMP:' + utcnow)
 
     #3.8.4.7. Unique Identifier
-    print ('UID:' + uid + '@teslations.ddns.net')
+    print ('UID:' + uid + '@ausrebellion.earth')
 
     # 3.8.7.1. Date-Time Created
     # The date and time that the calendar information was 
@@ -121,6 +156,7 @@ if response:
 
     #3.8.1.5. Description
     orig_description =  i['description']
+    
     # Remove HTML formatting codes
     mod_description1 = orig_description.replace("<p>", "")
     mod_description2 = mod_description1.replace("</p>", " ")
@@ -134,8 +170,16 @@ if response:
     mod_description10 = mod_description9.replace("<li>", " ")
     mod_description11 = mod_description10.replace("</li>", " ")
     mod_description12 = mod_description11.replace("<strong>", " ")
-    mod_description13 = mod_description12.replace("</strong>", " ")    
-    mod_description = mod_description13.replace("<br>", " ")
+    mod_description13 = mod_description12.replace("</strong>", " ")
+    mod_description14 = mod_description13.replace("&mdash;", " - ")    
+    mod_description = mod_description14.replace("<br>", "\r")
+
+# TO DO - Line length should not be longer than 75 characters.
+# RFC 5545 3.1. Content Lines
+# Nextcloud calendar description width is 47 characters.
+# (Don't end lines with CRLF)?
+#    print ('DESCRIPTION:', end='')
+   
     print ('DESCRIPTION:' + mod_description + '\r' + i['browser_url'])
 
     #3.8.7.3. Last Modified
@@ -178,12 +222,9 @@ if response:
 
     print ('END:VALARM')
     print ('END:VEVENT')
-
+#--------------------------------------------
   print ('END:VCALENDAR')
 
 else:
   print ('Request returned an error.')
   print (response.status_code)
-
-
-

0a0dba68a91eb72790265ea5c0faa81b1907c964
teslations <teslations@gmail.com>
2020-04-29 22:49:35 +1000

    Update

--- AN-events-export-NO_APIKEY.py
+++ AN-events-export-NO_APIKEY.py
@@ -14,25 +14,8 @@
 # iCalendar Validator
 # https://icalendar.org/validator.html
 #
-# You need to enter your API KEY and TZID in SETTINGS below to run this program
-
-#****************SETTINGS****************
-#******** Enter your API KEY here ********
-api_key = 'ENTER YOUR API KEY HERE'
-
-
-if api_key == 'ENTER YOUR API KEY HERE':
-    api_key = input(' Enter your API KEY : ')
-    print('''Enter your API KEY in the settings section
-    of this program, to store it permanently.
-    And your TimeZone ID (TZID). ''')
-
-# **** Enter your TimeZone ID here ****
-TZID = 'Australia/Brisbane'    
-    
-# **** ENDPOINT ****
-endpoint = 'https://actionnetwork.org/api/v2/events/'
- 
+# ENDPOINTS
+#
 #For a collection of all events associated with your API key
 # https://actionnetwork.org/api/v2/events
 #
@@ -42,7 +25,26 @@ endpoint = 'https://actionnetwork.org/api/v2/events/'
 #To address a specific event, use the identifier without the action_network: prefix to construct a URL, like
 # https://actionnetwork.org/api/v2/events/[id]
 # https://actionnetwork.org/api/v2/events/d91b4b2e-ae0e-4cd3-9ed7-d0ec501b0bc3
-#****************************************
+#
+#****************SETTINGS****************
+# Adjust these settings to run this program
+#
+#**** API KEY *****
+api_key = 'ENTER YOUR API KEY HERE'
+
+#
+# **** TimeZone ID ****
+TZID = 'Australia/Brisbane'    
+#   
+# **** ENDPOINT ****
+endpoint = 'https://actionnetwork.org/api/v2/events/'
+#
+#*******************************************
+
+if api_key == 'ENTER YOUR API KEY HERE':
+    api_key = input(' Enter your API KEY : ')
+    print('''Enter your API KEY in the settings section
+    of this program, to store it permanently.''')
 
 import requests
 import json
@@ -64,45 +66,48 @@ if response:
   #write JSON file to disk
   with open('AN-events.json', 'w') as json_file:
     json.dump(parsed, json_file, sort_keys=True, indent=4)
-
+  
+  #write calendar file
+  output_file=open('XRcalendar.ics', 'w')
+  
   #----CALENDAR----
-  print ('BEGIN:VCALENDAR')
-  print ('PRODID:-//XR//Calendar//EN')
-  print ('VERSION:2.0')
-  print ('CALSCALE:GREGORIAN')
-  print ('METHOD:PUBLISH')
-  print ('X-WR-CALNAME:XR')
-  print ('X-WR-TIMEZONE:' + TZID)
+  output_file.write ('BEGIN:VCALENDAR'+'\n')
+  output_file.write ('PRODID:-//XR//Calendar//EN'+'\n')
+  output_file.write ('VERSION:2.0'+'\n')
+  output_file.write ('CALSCALE:GREGORIAN'+'\n')
+  output_file.write ('METHOD:PUBLISH'+'\n')
+  output_file.write ('X-WR-CALNAME:XR'+'\n')
+  output_file.write ('X-WR-TIMEZONE:' + TZID+'\n')
 
   #----TIMEZONE----  
-  print ('BEGIN:VTIMEZONE')
+  output_file.write ('BEGIN:VTIMEZONE'+'\n')
   #3.2.19. Time Zone Identifier
   #  The "TZID" property parameter MUST NOT be applied to TIME
   #  properties whose time values are specified in UTC.
-  print ('TZID:' + TZID)
-  print ('X-LIC-LOCATION:' + TZID)
+  output_file.write ('TZID:' + TZID+'\n')
+  output_file.write ('X-LIC-LOCATION:' + TZID+'\n')
 
   #----STANDARD----
-  print ('BEGIN:STANDARD')
+  output_file.write ('BEGIN:STANDARD'+'\n')
   # 3.8.3.3. Time Zone Offset From. This property specifies the offset that is in use prior to this time zone observance. 
-  print ('TZOFFSETFROM:+1000')
+  output_file.write ('TZOFFSETFROM:+1000'+'\n')
   #3.8.3.4. Time Zone Offset To . This property specifies the offset that is in use in this time zone observance.
-  print ('TZOFFSETTO:+1000')
+  output_file.write ('TZOFFSETTO:+1000'+'\n')
   #3.8.3.2.  Time Zone Name
-  print ('TZNAME:AEST')
-  print ('DTSTART:19700101T000000Z')
-  print ('END:STANDARD')
+  output_file.write ('TZNAME:AEST'+'\n')
+  output_file.write ('DTSTART:19700101T000000Z'+'\n')
+  output_file.write ('END:STANDARD'+'\n')
   
-  print ('END:VTIMEZONE')
+  output_file.write ('END:VTIMEZONE'+'\n')
   
 #----EVENT-----------------------------------
   for i in events:
 
     #----VEVENT----
-    print ('BEGIN:VEVENT')
+    output_file.write ('BEGIN:VEVENT'+'\n')
 
     #3.8.2.4. Date-Time Start - when the calendar component begins
-    # Dates are entered as local time in AN but exported as GMT(Z) time. ??
+    # Dates are entered as local time in AN but exported as GMT(Z+'\n') time. ??
     orig_start_date = i['start_date']
     if str.endswith(orig_start_date,'Z'):
         d = datetime.datetime.strptime(orig_start_date, "%Y-%m-%dT%H:%M:%SZ")
@@ -110,26 +115,26 @@ if response:
         prefix_date=':'
     else:    
         prefix_date=';'+TZID+':'
-    print ('DSTART'+prefix_date+mod_start_date)
+    output_file.write ('DTSTART'+prefix_date+mod_start_date+'\n')
 
     #3.8.2.2. Date-Time End - the date and time that a calendar component ends
     if 'end_date' in events:
         orig_end_date = i['end_date']
         if str.endswith(orig_end_date,'Z'):
-            d = datetime.datetime.strptime(orig_end_date, "%Y-%m-%dT%H:%M:%S")
-            mod_end_date = d.strftime("%Y%m%dT%H%M%S")
+            d = datetime.datetime.strptime(orig_end_date, "%Y-%m-%dT%H:%M:%SZ")
+            mod_end_date = d.strftime("%Y%m%dT%H%M%SZ")
             prefix_date=':'
         else:    
             prefix_date=';'+TZID+':'
-        print ('DSTART'+prefix_date+mod_start_date)
+        output_file.write ('DTSTART'+prefix_date+mod_start_date+'\n')
     else:
         #if no end date: end date = start date
-        print('DTEND'+prefix_date+mod_start_date)
+        output_file.write('DTEND'+prefix_date+mod_start_date+'\n')
 
     #3.8.5.3. Recurrence Rule
 # TO DO - Check if Action Network has recurring events
 #    if 'rrule' in events:
-#        print ('RRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20200508T135959Z;BYDAY=FR')
+#        output_file.write ('RRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20200508T135959Z;BYDAY=FR')
 
     #----Current Date Time in 20200418T001100Z format
     now = datetime.datetime.now()
@@ -139,10 +144,10 @@ if response:
     
     #3.8.7.2. Date-Time Stamp
     #DTSTAMP:20200418T001100Z
-    print ('DTSTAMP:' + utcnow)
+    output_file.write ('DTSTAMP:' + utcnow+'\n')
 
     #3.8.4.7. Unique Identifier
-    print ('UID:' + uid + '@ausrebellion.earth')
+    output_file.write ('UID:' + uid + '@ausrebellion.earth'+'\n')
 
     # 3.8.7.1. Date-Time Created
     # The date and time that the calendar information was 
@@ -152,7 +157,7 @@ if response:
     # Create datetime object
     d = datetime.datetime.strptime(orig_created_date, "%Y-%m-%dT%H:%M:%SZ")
     mod_created_date = d.strftime("%Y%m%dT%H%M%SZ")
-    print ('CREATED:' + mod_created_date)
+    output_file.write ('CREATED:' + mod_created_date+'\n')
 
     #3.8.1.5. Description
     orig_description =  i['description']
@@ -171,60 +176,61 @@ if response:
     mod_description11 = mod_description10.replace("</li>", " ")
     mod_description12 = mod_description11.replace("<strong>", " ")
     mod_description13 = mod_description12.replace("</strong>", " ")
-    mod_description14 = mod_description13.replace("&mdash;", " - ")    
-    mod_description = mod_description14.replace("<br>", "\r")
+    mod_description14 = mod_description13.replace("&mdash;", " - ")
+    mod_description15 = mod_description14.replace("target=_blank>", " ")
+    mod_description = mod_description15.replace("<br>", "\r")
 
 # TO DO - Line length should not be longer than 75 characters.
 # RFC 5545 3.1. Content Lines
 # Nextcloud calendar description width is 47 characters.
-# (Don't end lines with CRLF)?
-#    print ('DESCRIPTION:', end='')
+# (Don't end lines with CRLF?
+#    output_file.write ('DESCRIPTION:', end='')
    
-    print ('DESCRIPTION:' + mod_description + '\r' + i['browser_url'])
+    output_file.write ('DESCRIPTION:' + mod_description + i['browser_url']+'\n')
 
     #3.8.7.3. Last Modified
     #Change format from 2019-12-05T04:03:06Z to 19960329T133000Z
     orig_modified_date = i['modified_date']
     x = orig_modified_date.replace("-", "")
     mod_modified_date = x.replace(":", "")
-    print ('LAST-MODIFIED:' + mod_modified_date)
+    output_file.write ('LAST-MODIFIED:' + mod_modified_date+'\n')
 
     #3.8.1.7. Location (The venue of the calendar event)
-    print ('LOCATION:' + i['location']['venue'],i['location']['address_lines'])
+    #output_file.write ('LOCATION:' + i['location']['venue'],i['location']['address_lines']+'\n')
 
     #3.8.7.4. Sequence Number
     # revision sequence number of the calendar component within a sequence of revisions.
-    print ('SEQUENCE:0')
+    output_file.write ('SEQUENCE:0'+'\n')
 
     #3.8.1.11. Status
     # overall status or confirmation for the calendar component.
-    print ('STATUS:' + i['status'].upper())
+    output_file.write ('STATUS:' + i['status'].upper()+'\n')
 
     #3.8.1.12. Summary
     # short summary or subject for the calendar component.
-    print ('SUMMARY:' + i['title'])
+    output_file.write ('SUMMARY:' + i['title']+'\n')
 
     #3.8.2.7. Time Transparency
     # whether or not an event is transparent to busy time searches.
-    print ('TRANSP:' + i['transparence'].upper())
+    output_file.write ('TRANSP:' + i['transparence'].upper()+'\n')
 
     #----VALARM----
-    print ('BEGIN:VALARM')
+    output_file.write ('BEGIN:VALARM'+'\n')
 
     #3.8.6.1. Action
     # the action to be invoked when an alarm is triggered. (AUDIO or DISPLAY)
-    print ('ACTION:DISPLAY')
+    output_file.write ('ACTION:DISPLAY'+'\n')
 
-    print ('DESCRIPTION:This is an event reminder')
+    output_file.write ('DESCRIPTION:This is an event reminder'+'\n')
 
     #3.8.6.3. Trigger - when an alarm will trigger.
-    print ('TRIGGER:-P0DT4H0M0S')
+    output_file.write ('TRIGGER:-P0DT4H0M0S'+'\n')
 
-    print ('END:VALARM')
-    print ('END:VEVENT')
+    output_file.write ('END:VALARM'+'\n')
+    output_file.write ('END:VEVENT'+'\n')
 #--------------------------------------------
-  print ('END:VCALENDAR')
-
+  output_file.write ('END:VCALENDAR'+'\n')
+  output_file.close()
 else:
   print ('Request returned an error.')
   print (response.status_code)
